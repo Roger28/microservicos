ecfefc0b82e518f9f7d46e98e08ec6475bf9ef5c
roger28 <rogersouza635@gmail.com>
2021-03-07 11:10:11 -0300

    Implementação com feign client.

--- hr-payroll/src/main/java/com/rwork/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/rwork/payroll/services/PaymentService.java
@@ -1,15 +1,12 @@
 package com.rwork.payroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.rwork.payroll.entities.Payment;
 import com.rwork.payroll.entities.Worker;
+import com.rwork.payroll.feignclients.WorkerFeignClient;
 import com.rwork.payroll.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -19,14 +16,11 @@ public class PaymentService {
 	private String workerHost;
 
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
 	public Payment getPayment(Long workerId, Integer days) {
 		try {
-			Map<String, String> params = new HashMap<>();
-			params.put("id", String.valueOf(workerId));
-
-			Worker worker = this.restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, params);
+			Worker worker = this.workerFeignClient.findById(workerId).getBody();
 			return new Payment(worker.getName(), worker.getDailyIncome(), days);
 		} catch (Exception e) {
 			throw new ResourceNotFoundException(workerId);
