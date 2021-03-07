871494c86a4e38890afe041372388c7efa725117
roger28 <rogersouza635@gmail.com>
2021-03-07 12:48:37 -0300

    Projeto hr-gateway-zuul criado.

---
+++ hr-gateway-zuul/src/main/java/com/rwork/zuul/HrGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.rwork.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrGatewayZuulApplication.class, args);
+	}
+
+}
