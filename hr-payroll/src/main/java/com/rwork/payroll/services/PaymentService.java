701b57953fb2bdbf37c99e1eee61879528e35160
roger28 <rogersouza635@gmail.com>
2021-03-07 10:53:29 -0300

    Implementação com rest template.

--- hr-payroll/src/main/java/com/rwork/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/rwork/payroll/services/PaymentService.java
@@ -1,13 +1,35 @@
 package com.rwork.payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.rwork.payroll.entities.Payment;
+import com.rwork.payroll.entities.Worker;
+import com.rwork.payroll.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class PaymentService {
-	
+
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Roger", 200.0, days);
+		try {
+			Map<String, String> params = new HashMap<>();
+			params.put("id", String.valueOf(workerId));
+
+			Worker worker = this.restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, params);
+			return new Payment(worker.getName(), worker.getDailyIncome(), days);
+		} catch (Exception e) {
+			throw new ResourceNotFoundException(workerId);
+		}
 	}
 }
